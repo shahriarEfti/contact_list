f1ebdff1ea49fec4d4eac367e699cf99128424c7
MD SHAHRIAR PERVEZ EFTI <cse049.shahriarefti@gmail.com>
2024-06-07 15:50:44 +0600

    add custom AppBar

--- lib/ui/Screens/home_screen.dart
+++ lib/ui/Screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:contact_list/ui/Widgets/custom_appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
@@ -14,204 +15,200 @@ class _HomeScreenState extends State<HomeScreen> {
   final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
 
   List<Map<String, String>> _contacts = [];
+  AutovalidateMode _autovalidateMode = AutovalidateMode.disabled;
 
   void _addContact() {
-    if (_formkey.currentState!.validate()) {
-      setState(() {
-        _contacts.add({
-          'name': _nameTEcontroller.text,
-          'number': _numberTEcontroller.text,
+    setState(() {
+      _autovalidateMode = AutovalidateMode.onUserInteraction;
+    });
+
+
+      if (_formkey.currentState!.validate()) {
+        setState(() {
+          _contacts.add({
+            'name': _nameTEcontroller.text,
+            'number': _numberTEcontroller.text,
+          });
+          _nameTEcontroller.clear();
+          _numberTEcontroller.clear();
+          _autovalidateMode = AutovalidateMode.disabled;
         });
-        _nameTEcontroller.clear();
-        _numberTEcontroller.clear();
+      }
+    }
+
+    void _deleteContact(int index) {
+      setState(() {
+        _contacts.removeAt(index);
       });
     }
-  }
 
+    Future<void> _showDeleteConfirmationDialog(int index) async {
+      return showDialog<void>(
+        context: context,
+        barrierDismissible: false, // user must tap button!
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Confirm'),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[
+                  Text('Are you sure you want to delete this contact?'),
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: Text('Cancel'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              TextButton(
+                child: Text('Delete'),
+                onPressed: () {
+                  _deleteContact(index);
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
 
-  void _deleteContact(int index) {
-    setState(() {
-      _contacts.removeAt(index);
-    });
-  }
+    @override
+    void dispose() {
+      _nameTEcontroller.dispose();
+      _numberTEcontroller.dispose();
+      super.dispose();
+    }
 
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: CustomAppBar(title: "Contact List"),
+        body: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Form(
+            key: _formkey,
+            autovalidateMode: _autovalidateMode,
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _nameTEcontroller,
+                  decoration: InputDecoration(
+                    hintText: "Name",
+                    labelText: "Name",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
 
+                  validator: (String? value) {
+                    if (value
+                        ?.trim()
+                        .isEmpty ?? true) {
+                      return "Please enter the Name";
+                    }
+                    return null;
+                  },
+                ),
+                Gap(10),
+                TextFormField(
+                  controller: _numberTEcontroller,
+                  decoration: InputDecoration(
+                    hintText: "Number",
+                    labelText: "Number",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
 
-
-  Future<void> _showDeleteConfirmationDialog(int index) async {
-    return showDialog<void>(
-      context: context,
-      barrierDismissible: false, // user must tap button!
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Confirm'),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                Text('Are you sure you want to delete this contact?'),
+                  validator: (String? value) {
+                    if (value
+                        ?.trim()
+                        .isEmpty ?? true) {
+                      return "Please enter the Number";
+                    }
+                    return null;
+                  },
+                ),
+                Gap(20),
+                SizedBox(
+                  width: MediaQuery
+                      .of(context)
+                      .size
+                      .width,
+                  child: ElevatedButton(
+                    onPressed: _addContact,
+                    child: Text("Add"),
+                  ),
+                ),
+                Gap(20),
+                Expanded(
+                  child: ListView.separated(
+                    itemCount: _contacts.length,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                        onLongPress: () {
+                          _showDeleteConfirmationDialog(index);
+                        },
+                        child: ListTile(
+                          title: Text(
+                            _contacts[index]['name']!,
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          subtitle: Text(
+                            _contacts[index]['number']!,
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.grey[600],
+                            ),
+                          ),
+                          leading: CircleAvatar(
+                            backgroundColor: Colors.blue,
+                            child: Icon(
+                              Icons.person,
+                              color: Colors.white,
+                            ),
+                          ),
+                          trailing: IconButton(
+                            icon: Icon(
+                              Icons.call,
+                              color: Colors.green,
+                            ),
+                            onPressed: () {
+                              // Handle call action
+                            },
+                          ),
+                          tileColor: Colors.grey[200],
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 16.0, vertical: 8.0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return Divider(
+                        color: Colors.grey[400],
+                        height: 1,
+                        thickness: 1,
+                        indent: 16,
+                        endIndent: 16,
+                      );
+                    },
+                  ),
+                ),
               ],
             ),
           ),
-          actions: <Widget>[
-            TextButton(
-              child: Text('Cancel'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              child: Text('Delete'),
-              onPressed: () {
-                _deleteContact(index);
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
+        ),
+      );
+    }
   }
 
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Center(
-          child: Text(
-            "Contact List",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(12),
-        child: Form(
-          key: _formkey,
-          child: Column(
-            children: [
-              TextFormField(
-                controller: _nameTEcontroller,
-                decoration: InputDecoration(
-                  hintText: "Name",
-                  labelText: "Name",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                ),
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                validator: (String? value) {
-                  if (value?.trim().isEmpty ?? true) {
-                    return "Please enter the Name";
-                  }
-                  return null;
-                },
-              ),
-              Gap(10),
-              TextFormField(
-                controller: _numberTEcontroller,
-                decoration: InputDecoration(
-                  hintText: "Number",
-                  labelText: "Number",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                ),
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                validator: (String? value) {
-                  if (value?.trim().isEmpty ?? true) {
-                    return "Please enter the Number";
-                  }
-                  return null;
-                },
-              ),
-              Gap(20),
-              SizedBox(
-                width: MediaQuery.of(context).size.width,
-                child: ElevatedButton(
-                  onPressed: _addContact,
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.white,
-                    backgroundColor: Colors.blue,
-                    padding: EdgeInsets.symmetric(vertical: 16.0),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    elevation: 5,
-                  ),
-                  child: Text("Add"),
-                ),
-              ),
-              Gap(20),
-              Expanded(
-                child: ListView.separated(
-                  itemCount: _contacts.length,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                        onLongPress: () {
-                      _showDeleteConfirmationDialog(index);
-                    },
-
-                      child:ListTile(
-                      title: Text(
-                        _contacts[index]['name']!,
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      subtitle: Text(
-                        _contacts[index]['number']!,
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          color: Colors.grey[600],
-                        ),
-                      ),
-                      leading: CircleAvatar(
-                        backgroundColor: Colors.blue,
-                        child: Icon(
-                          Icons.person,
-                          color: Colors.white,
-                        ),
-                      ),
-                      trailing: IconButton(
-                        icon: Icon(
-                          Icons.call,
-                          color: Colors.green,
-                        ),
-                        onPressed: () {
-                          // Handle call action
-                        },
-                      ),
-                      tileColor: Colors.grey[200],
-                      contentPadding: EdgeInsets.symmetric(
-                          horizontal: 16.0, vertical: 8.0),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      ),);
-                  },
-                  separatorBuilder: (context, index) {
-                    return Divider(
-                      color: Colors.grey[400],
-                      height: 1,
-                      thickness: 1,
-                      indent: 16,
-                      endIndent: 16,
-                    );
-                  },
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
